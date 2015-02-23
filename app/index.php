d6c511ca4fe4a6fbe73b59e4f094e5db4d8edf3a
Vedran Jaic <vedran@404.agency>
2015-02-23 14:21:04 +0100

    Added config/parse_path.php for pretty URLs

--- app/index.php
+++ app/index.php
@@ -1,5 +1,21 @@
-<?php include('header.php'); ?>
+<?php
+include('config/parse_path.php');
+include('header.php');
+?>
 
-	<h1>Hello, world!</h1>
+	<?php
+
+		// Call parts
+		if($path["call_parts"][0] != ""){
+			if(file_exists($path["call_parts"][0].'.php')){
+				include($path["call_parts"][0].'.php');
+			} else {
+				include('404.php');
+			}
+		} else {
+			include('homepage.php');
+		}
+
+	?>
 
 <?php include('footer.php'); ?>
